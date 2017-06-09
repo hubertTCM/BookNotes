d84f43756177ce51734a065dc14f7e5a3db7792b
HubertTCM <fuqunxing@gmail.com>
2017-06-09 22:18:56 +1200

    display next section when scroll to the end

--- Android/app/src/main/java/android/hubert/com/booknotes/SectionUI.java
+++ Android/app/src/main/java/android/hubert/com/booknotes/SectionUI.java
@@ -3,6 +3,7 @@ package android.hubert.com.booknotes;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.widget.AbsListView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -16,6 +17,7 @@ import com.hubert.dal.entity.SectionEntity;
 
 public class SectionUI extends LinearLayout {
     private SectionEntity mEntity;
+    ListView mBlockView;
 
     public SectionUI(Context context) {
         super(context);
@@ -29,21 +31,46 @@ public class SectionUI extends LinearLayout {
 
     public void setEntity(SectionEntity entity) {
         mEntity = findLowestSection(entity);
+        if (mEntity == null) {
+            return;
+        }
 
         TextView titleView = (TextView) findViewById(R.id.textViewTitle);
         titleView.setText(mEntity.name);
 
-        ListView blockView = (ListView)findViewById(R.id.listViewBlock);
         BlockAdapter adapter = new BlockAdapter(getContext(), mEntity.blocks);
-        blockView.setAdapter(adapter);
+        mBlockView.setAdapter(adapter);
     }
 
     private void setUp(Context context) {
         LayoutInflater inflater = LayoutInflater.from(context);
         inflater.inflate(R.layout.section, this);
+
+        mBlockView = (ListView) findViewById(R.id.listViewBlock);
+
+        mBlockView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if (firstVisibleItem + visibleItemCount < totalItemCount || totalItemCount == 0) {
+                    return;
+                }
+
+                SectionEntity next = findNextSection();
+                setEntity(next);
+            }
+        });
     }
 
-    private SectionEntity findLowestSection(SectionEntity entity) {
+    private static SectionEntity findLowestSection(SectionEntity entity) {
+        if (entity == null){
+            return null;
+        }
+
         if (!entity.blocks.isEmpty()) {
             return entity;
         }
@@ -54,6 +81,37 @@ public class SectionUI extends LinearLayout {
                 return temp;
             }
         }
+        return null;
+    }
+
+    private SectionEntity findNextSection() {
+        if (mEntity.parent != null) {
+            for (SectionEntity temp : mEntity.parent.childSections) {
+                if (temp.order > mEntity.order) {
+                    return temp;
+                }
+            }
+        }
+
+        BookEntity book = mEntity.book;
+        SectionEntity parent = mEntity;
+        while(book == null || book.sections == null || book.sections.isEmpty()){
+            parent = parent.parent;
+            if (parent == null){
+                return null;
+            }
+            book = parent.book;
+        }
+
+        for (SectionEntity temp : book.sections) {
+            if (temp.parent != null) {
+                continue;
+            }
+            if (temp.order > mEntity.order) {
+                return temp;
+            }
+        }
+
         return null;
     }
 
