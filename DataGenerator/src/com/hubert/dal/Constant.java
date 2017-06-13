efc486610498ec3c984574a6e7560e4758fddc6c
hubertTCM <fuqunxing@gmail.com>
2017-06-13 21:33:18 +1200

    move the constant.java to DataGenerator project

---
+++ DataGenerator/src/com/hubert/dal/Constant.java
@@ -0,0 +1,11 @@
+package com.hubert.dal;
+
+public class Constant {
+	// https://bitbucket.org/xerial/sqlite-jdbc
+	public static String DATABASE_URL = "jdbc:sqlite:db/tcm.db";
+	
+	public static String FILED_NAME = "name";
+	
+	public static String FILED_ALIAS = "alias";
+	public static String FIELD_PRESCRIPTION_ID = "prescription_id";
+}
