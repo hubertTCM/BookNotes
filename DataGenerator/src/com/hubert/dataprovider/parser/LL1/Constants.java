a9437d796688c7ce900a97d8c57f125697a4c47e
Hubert <fuqunxing@gmail.com>
2017-09-03 00:14:52 +1200

    implement calculateFollowSet

--- DataGenerator/src/com/hubert/dataprovider/parser/LL1/Constants.java
+++ DataGenerator/src/com/hubert/dataprovider/parser/LL1/Constants.java
@@ -2,4 +2,6 @@ package com.hubert.dataprovider.parser.LL1;
 
 public class Constants {
 	public static final String Empty = "Empty";
+	public static final String End = "$";
+	public static final String Start = "S";
 }
