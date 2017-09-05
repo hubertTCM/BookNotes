5505e37fcbfe7be4b73d33b92ed9d2abf00bf5e4
HubertTCM <fuqunxing@gmail.com>
2017-09-05 21:16:24 +1200

    add a test case of ll1

--- DataGenerator/src/com/hubert/dataprovider/importor.java
+++ DataGenerator/src/com/hubert/dataprovider/importor.java
@@ -19,49 +19,9 @@ public class importor {
 			// com.hubert.dataprovider.parser.LL1.YiAnParser parser = new
 			// com.hubert.dataprovider.parser.LL1.YiAnParser(
 			// "resource/临证指南医案/format_ignore.txt");
-			// S –> AB
-			// A –> Ca | ε
-			// B –> cB'
-			// B' –> aACB' | ε
-			// C –> b | ε
 
-			ArrayList<String> expressions = new ArrayList<String>();
-			expressions.add("S := A B");
-			expressions.add("A := C a");
-			expressions.add("A := Empty");
-			expressions.add("B := c B'");
-			expressions.add("B' := a A C B'");
-			expressions.add("B' := Empty");
-			expressions.add("C := b");
-			expressions.add("C := Empty");
-			ArrayList<String> terminalSymbols = new ArrayList<String>();
-			terminalSymbols.add("a");
-			terminalSymbols.add("c");
-			terminalSymbols.add("b");
+			testCase2();
 
-			com.hubert.dataprovider.parser.LL1.YiAnParser parser2 = new com.hubert.dataprovider.parser.LL1.YiAnParser(
-					terminalSymbols, expressions);
-			parser2.parse(null);
-			// 计算结果如下：
-			//
-			// First(C) = {b, ε}
-			// First(B') = {a, ε}
-			// First(B) = {c}
-			// First(A) = {b, a, ε}
-			// First(S) = {b, a, c}
-			//
-			// Follow(S) = {$}
-			// Follow(B) = {$}
-			// Follow(B') = {$}
-			// Follow(C) = {a, $}
-			// Follow(A) = {c, b, a, $}
-
-			// BookGenerator generator = new BookGenerator("临证指南医案");
-			// generator.doImport();
-
-			// HerbAliasManager aliasManager = new HerbAliasManager();
-			// aliasManager.load();
-			// HerbAliasManager.getInstance().trace();
 			System.out.println("done");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -72,4 +32,80 @@ public class importor {
 		}
 	}
 
+	public static void testCase1() throws Exception {
+
+		// S –> AB
+		// A –> Ca | ε
+		// B –> cB'
+		// B' –> aACB' | ε
+		// C –> b | ε
+
+		ArrayList<String> expressions = new ArrayList<String>();
+		expressions.add("S := A B");
+		expressions.add("A := C a");
+		expressions.add("A := Empty");
+		expressions.add("B := c B'");
+		expressions.add("B' := a A C B'");
+		expressions.add("B' := Empty");
+		expressions.add("C := b");
+		expressions.add("C := Empty");
+		ArrayList<String> terminalSymbols = new ArrayList<String>();
+		terminalSymbols.add("a");
+		terminalSymbols.add("c");
+		terminalSymbols.add("b");
+
+		com.hubert.dataprovider.parser.LL1.YiAnParser parser2 = new com.hubert.dataprovider.parser.LL1.YiAnParser(
+				terminalSymbols, expressions);
+		parser2.parse(null);
+		// 计算结果如下：
+		//
+		// First(C) = {b, ε}
+		// First(B') = {a, ε}
+		// First(B) = {c}
+		// First(A) = {b, a, ε}
+		// First(S) = {b, a, c}
+		//
+		// Follow(S) = {$}
+		// Follow(B) = {$}
+		// Follow(B') = {$}
+		// Follow(C) = {a, $}
+		// Follow(A) = {c, b, a, $}
+
+		// BookGenerator generator = new BookGenerator("临证指南医案");
+		// generator.doImport();
+
+		// HerbAliasManager aliasManager = new HerbAliasManager();
+		// aliasManager.load();
+		// HerbAliasManager.getInstance().trace();
+	}
+
+	public static void testCase2() throws Exception {
+		// S -> TE'
+		// E' -> +TE' | ε
+		// T -> FT'
+		// T' -> *FT' | ε
+		// F -> (S) | id
+
+		ArrayList<String> expressions = new ArrayList<String>();
+		expressions.add("S  := T E'");
+		expressions.add("E' := + T E'");
+		expressions.add("E'  := Empty");
+		expressions.add("T  := F T'");
+		expressions.add("T'  := * F T'");
+		expressions.add("T'  := Empty");
+		expressions.add("F  := ( S )");
+		expressions.add("F  := id");
+		ArrayList<String> terminalSymbols = new ArrayList<String>();
+		terminalSymbols.add("(");
+		terminalSymbols.add(")");
+		terminalSymbols.add("id");
+		terminalSymbols.add("+");
+		terminalSymbols.add("*");
+		terminalSymbols.add("Empty");
+
+		com.hubert.dataprovider.parser.LL1.YiAnParser parser2 = new com.hubert.dataprovider.parser.LL1.YiAnParser(
+				terminalSymbols, expressions);
+		parser2.parse(null);
+
+	}
 }
