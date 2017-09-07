b9df94496563f175d7820e0eb0e2a49bbafd75ac
HubertTCM <fuqunxing@gmail.com>
2017-09-07 20:34:17 +1200

    start on LL1 Parser

--- DataGenerator/src/com/hubert/dataprovider/parser/LL1/ASTNode.java
+++ DataGenerator/src/com/hubert/dataprovider/parser/LL1/ASTNode.java
@@ -5,28 +5,57 @@ import java.util.*;
 import com.hubert.dataprovider.parser.tokenextractor.*;
 
 public class ASTNode {
-	public ASTNode(Token token){
-		mToken = token;
+	public ASTNode(String tag) {
+		this(tag, "");
 	}
-	
-	public Token getToken(){
-		return mToken;
+
+	public ASTNode(String tag, String value) {
+		mValue = value;
+		mTag = tag;
 	}
-	
-	public String getTag(){
+
+	public String getTag() {
 		return mTag;
 	}
-	
-	public void addChild(ASTNode child){
-		mChildren.add(child);
-		if (child.mParent != null){
-			// TODO:
-		}
-		child.mParent = this; 
+
+	public String getValue() {
+		return mValue;
+	}
+
+	public void setValue(String value) {
+		mValue = value;
 	}
 	
+	public ASTNode getParent(){
+		return mParent;
+	}
+
+	public void addChild(ASTNode child) {
+		mChildren.add(child);
+		if (child.mParent != null) {
+			// TODO:
+		}
+		child.mParent = this;
+	}
+
+	public boolean replaceChild(ASTNode oldNode, ASTNode newNode) {
+		if (newNode.mParent != null) {
+			return false;
+		}
+		int index = mChildren.indexOf(oldNode);
+		if (index < 0) {
+			return false;
+		}
+		mChildren.remove(index);
+		oldNode.mParent = null;
+
+		newNode.mParent = this;
+		mChildren.add(index, newNode);
+		return true;
+	}
+
 	private String mTag;
-	private Token mToken;
+	private String mValue;
 	private List<ASTNode> mChildren = new ArrayList<ASTNode>();
 	private ASTNode mParent;
 }
