ae972b292188d59a78905284593712141c900f3e
hubertTCM <fuqunxing@gmail.com>
2017-09-26 20:09:31 +1300

    Render AST by d3.

---
+++ UI/data.js
@@ -0,0 +1,120 @@
+var data1 = 
+
+{"tag":"Root",
+ "value":"",
+ "name":"",
+ "children": [
+    {"tag":"S",
+     "value":"",
+     "name":"",
+     "children": [
+        {"tag":"YN",
+         "value":"",
+         "name":"",
+         "children": [
+            {"tag":"YNDetail",
+             "value":"",
+             "name":"",
+             "children": [
+                {"tag":"Description",
+                 "value":"又 今年天符岁会，上半年阳气大泄，.....",
+                 "name":"又 今年天符岁会，上半年阳气大泄，.....",
+                 "children": []
+                },
+                {"tag":"RecipeDetail",
+                 "value":"",
+                 "name":"",
+                 "children": [
+                    {"tag":"RecipeContent",
+                     "value":"",
+                     "name":"",
+                     "children": [
+                        {"tag":"Herb",
+                         "value":"人参",
+                         "name":"人参",
+                         "children": []
+                        },
+                        {"tag":"RecipeCompositionHerbOnly",
+                         "value":"",
+                         "name":"",
+                         "children": [
+                            {"tag":"Herb",
+                             "value":"生牡蛎",
+                             "name":"生牡蛎",
+                             "children": []
+                            },
+                            {"tag":"RecipeCompositionHerbOnly",
+                             "value":"",
+                             "name":"",
+                             "children": [
+                                {"tag":"Herb",
+                                 "value":"生白芍",
+                                 "name":"生白芍",
+                                 "children": []
+                                }
+                                ]
+                            }
+                            ]
+                        }
+                        ]
+                    },
+                    {"tag":"AfterRecipeContent",
+                     "value":"",
+                     "name":"",
+                     "children": [
+                        {"tag":"RecipeComment",
+                         "value":"上午服。",
+                         "name":"上午服。",
+                         "children": []
+                        },
+                        {"tag":"RecipeDetail",
+                         "value":"",
+                         "name":"",
+                         "children": [
+                            {"tag":"RecipeContent",
+                             "value":"",
+                             "name":"",
+                             "children": [
+                                {"tag":"Herb",
+                                 "value":"人参",
+                                 "name":"人参",
+                                 "children": []
+                                },
+                                {"tag":"RecipeCompositionHerbOnly",
+                                 "value":"",
+                                 "name":"",
+                                 "children": [
+                                    {"tag":"Herb",
+                                     "value":"茯苓",
+                                     "name":"茯苓",
+                                     "children": []
+                                    }
+                                    ]
+                                }
+                                ]
+                            },
+                            {"tag":"AfterRecipeContent",
+                             "value":"",
+                             "name":"",
+                             "children": [
+                                {"tag":"RecipeComment",
+                                 "value":"为末，竹沥法丸，早上服三钱，百滚汤下。",
+                                 "name":"为末，竹沥法丸，早上服三钱，百滚汤下。",
+                                 "children": []
+                                }
+                                ]
+                            }
+                            ]
+                        }
+                        ]
+                    }
+                    ]
+                }
+                ]
+            }
+            ]
+        }
+        ]
+    }
+    ]
+}
