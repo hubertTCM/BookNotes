054e4cd8d93189e02edf9324d79704f36302a9e6
HubertTCM <fuqunxing@gmail.com>
2017-11-02 20:59:31 +1300

    delete unused code

--- DataGenerator/src/com/hubert/parser/tokenextractor/ITokenExtractor.java
+++ DataGenerator/src/com/hubert/parser/tokenextractor/ITokenExtractor.java
@@ -5,7 +5,5 @@ import java.util.*;
 import javafx.util.Pair;
 
 public interface ITokenExtractor {
-    // public Pair<Token, String> extract(String source);
-    // public Collection<Token> extract(String source);
     public Pair<Boolean, String> extract(String text, List<Token> container);
 }
