56a1225258f272c8bb9534526a2e1b6c38e9c1bc
HubertTCM <fuqunxing@gmail.com>
2017-11-11 05:09:14 +1300

    input End tag to spit YiAn in token file

--- DataGenerator/src/com/hubert/parser/tokenextractor/Constants.java
+++ DataGenerator/src/com/hubert/parser/tokenextractor/Constants.java
@@ -1,8 +1,11 @@
-package com.hubert.parser.LL1;
+package com.hubert.parser.tokenextractor;
 
 public class Constants {
     // should match to TokenType
     public static final String Empty = "Empty";
     public static final String End = "End";
     public static final String Start = "S";
+    
+    public static final String TokenStartTag = "[";
+    public static final String TokenEndTag = "]";
 }
