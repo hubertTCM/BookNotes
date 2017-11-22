2378cd8b28dee44a78f7e7b0b5c39f8f11991cf3
HubertTCM <fuqunxing@gmail.com>
2017-11-22 19:14:33 +1300

    build the FP tree

--- Algorithm/src/com/hubert/machinelearning/FPTree.java
+++ Algorithm/src/com/hubert/machinelearning/FPTree.java
@@ -5,15 +5,73 @@ import javafx.util.*;
 
 // http://www.cnblogs.com/pinard/p/6307064.html
 public class FPTree<T> {
-    public List<Pair<Integer, Collection<T>>> build(Collection<Collection<T>> source) {
-        // TODO: sort source
-        for (Collection<T> temp : source) {
+    public FPTree(Collection<Collection<T>> source, int lowerLimit) {
+        mSource = source;
+        mLowerLimit = lowerLimit;
+        build();
+    }
+
+    public List<Pair<Integer, List<T>>> get(T value, int limit) {
+        HeadNode<T> head = findHead(value);
+        return get(head, limit);
+    }
+
+    public List<Pair<Integer, List<T>>> getAll(int limit) {
+        List<Pair<Integer, List<T>>> result = new ArrayList<Pair<Integer, List<T>>>();
+        for (HeadNode<T> head : mHeads) {
+            result.addAll(get(head, limit));
+        }
+        return result;
+    }
+
+    private List<Pair<Integer, List<T>>> get(HeadNode<T> head, int lowerLimit) {
+        List<Pair<Integer, List<T>>> result = new ArrayList<Pair<Integer, List<T>>>();
+        head.merge(result, lowerLimit);
+        return result;
+    }
+
+    private void build() {
+        for (Collection<T> temp : mSource) {
             buildHead(temp);
         }
-        for (Collection<T> temp : source) {
-            updateTree(temp);
-        }
+        adjustHeads();
 
+        for (Collection<T> item : mSource) {
+            updateTree(item);
+        }
+    }
+
+    protected void sort(ArrayList<T> temp) {
+        temp.sort(new Comparator<T>() {
+
+            @Override
+            public int compare(T o1, T o2) {
+                HeadNode<T> h1 = findHead(o1);
+                HeadNode<T> h2 = findHead(o2);
+                if (h1.getCount() > h2.getCount()) {
+                    return 1;
+                }
+                if (h1.getCount() < h2.getCount()) {
+                    return -1;
+                }
+                return 0;
+            }
+        });
+    }
+
+    private void adjustHeads() {
+        sortHeads();
+        for (int i = mHeads.size() - 1; i >= 0; --i) {
+            HeadNode<T> head = mHeads.get(i);
+            if (head.getCount() < mLowerLimit) {
+                mHeads.remove(i);
+                continue;
+            }
+            break;
+        }
+    }
+
+    protected void sortHeads() {
         mHeads.sort(new Comparator<HeadNode<T>>() {
             @Override
             public int compare(FPTree<T>.HeadNode<T> o1, FPTree<T>.HeadNode<T> o2) {
@@ -27,9 +85,6 @@ public class FPTree<T> {
                 return 0;
             }
         });
-
-        List<Pair<Integer, Collection<T>>> result = new ArrayList<Pair<Integer, Collection<T>>>();
-        return result;
     }
 
     private HeadNode<T> findHead(T data) {
@@ -59,9 +114,16 @@ public class FPTree<T> {
     }
 
     private void updateTree(Collection<T> source) {
+        ArrayList<T> temp = new ArrayList<T>();
+        temp.addAll(source);
+        sort(temp);
+
         TreeNode<T> currentNode = mRoot;
-        for (T data : source) {
+        for (T data : temp) {
             HeadNode<T> head = findHead(data);
+            if (head == null) {
+                break;
+            }
             TreeNode<T> child = currentNode.findChild(data);
             if (child == null) {
                 child = currentNode.addChild(data);
@@ -101,6 +163,10 @@ public class FPTree<T> {
             super(value, 1);
         }
 
+        public TreeNode<T> getParent() {
+            return mParent;
+        }
+
         public TreeNode<T> findChild(T value) {
             for (TreeNode<T> child : mChildren) {
                 if (child.getValue() == value) {
@@ -131,8 +197,11 @@ public class FPTree<T> {
             return mNext;
         }
 
-        public TreeNode<T> getValue() {
-            return mValue;
+        public List<Pair<Integer, List<T>>> getAllItems() {
+            List<Pair<Integer, List<T>>> result = new ArrayList<Pair<Integer, List<T>>>();
+            //List<T> current = new ArrayList<T>();
+            //ListNode<T> currentNode = mNext;
+            return result;
         }
 
         public int getCount() {
@@ -146,6 +215,17 @@ public class FPTree<T> {
             return node;
         }
 
+        // public void swap(ListNode<T> to){
+        // if (to == null){
+        // return;
+        // }
+        //
+        // // just change the data value is OK here
+        // TreeNode<T> temp = mValue;
+        // mValue = to.getValue();
+        // to.mValue = temp;
+        // }
+
         TreeNode<T> mValue;
         ListNode<T> mNext;
     }
@@ -156,14 +236,32 @@ public class FPTree<T> {
             mNode = null;
         }
 
-        public ListNode<T> getNode() {
-            return mNode;
+        public void merge(List<Pair<Integer, List<T>>> source, int lowerLimit) {
+            if (mCount < lowerLimit) {
+                return;
+            }
+
+            ListNode<T> node = mNode;
+            while (node != null) {
+                List<Pair<Integer, List<T>>> temp = node.getAllItems();
+                merge(source, temp);
+                node = node.getNext();
+            }
         }
 
-        @Override
-        public void increaseCount() {
-            super.increaseCount();
-            // TODO: ensure the list is sort by count desc
+        private void merge(List<Pair<Integer, List<T>>> to, List<Pair<Integer, List<T>>> from) {
+            for (int i = 0; i < from.size(); i++) {
+                for (int j = 0; j < to.size(); j++) {
+                    Pair<Integer, List<T>> pairI = from.get(i);
+                    List<T> valueI = pairI.getValue();
+                    Pair<Integer, List<T>> pairJ = to.get(j);
+                    List<T> valueJ = pairJ.getValue();
+                    if (!valueI.containsAll(valueJ) || !valueJ.contains(valueI)) {
+                        continue;
+                    }
+                    to.set(j, new Pair<>(pairI.getKey() + pairJ.getKey(), valueJ));
+                }
+            }
         }
 
         public void insert(TreeNode<T> node) {
@@ -187,4 +285,6 @@ public class FPTree<T> {
 
     private List<HeadNode<T>> mHeads = new ArrayList<HeadNode<T>>();
     private TreeNode<T> mRoot = new TreeNode<T>(null);
+    private Collection<Collection<T>> mSource;
+    private int mLowerLimit = 0;
 }
