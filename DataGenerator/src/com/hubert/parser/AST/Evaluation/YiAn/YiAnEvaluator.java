136fc3337591c0500bcb1ea9591abc1580d74720
Hubert <fuqunxing@gmail.com>
2017-12-18 20:23:43 +1300

    delete unused code

--- DataGenerator/src/com/hubert/parser/AST/Evaluation/YiAn/YiAnEvaluator.java
+++ DataGenerator/src/com/hubert/parser/AST/Evaluation/YiAn/YiAnEvaluator.java
@@ -25,41 +25,5 @@ public class YiAnEvaluator extends AbstractEvaluator {
         return true;
     }
 
-    // TODO: it is tricky here. can be implemented better.
-//    @Override
-//    public boolean postEvaluate(ASTNode node) {
-//        //SectionEntity section = mYiAnScope.getActiveSection();
-//        //YiAnEntity yiAn = mYiAnScope.getYiAn().get();
-//        YiAnEntity yiAn = mYiAnScope.getYiAn();
-//        List<YiAnDetailBlockLinkEntity> tempLinks = new ArrayList<YiAnDetailBlockLinkEntity>();
-//        for (YiAnDetailEntity detail : yiAn.details) {
-//            tempLinks.clear();
-//
-//            tempLinks.addAll(detail.blockLinks);
-//
-//
-//            for (int j = tempLinks.size() - 1; j > 0; j--) {
-//                YiAnDetailBlockLinkEntity linkJ = tempLinks.get(j);
-//                for (int i = 0; i < j; i++) {
-//                    YiAnDetailBlockLinkEntity linkI = tempLinks.get(i);
-//                    if (linkJ.block.content.equals(linkI.block.content)) {
-//                        tempLinks.remove(j);
-//                        detail.blockLinks.remove(linkJ);
-//                    }
-//                }
-//            }
-//
-//            /*
-//            for (YiAnDetailBlockLinkEntity link : tempLinks) {
-//                BlockEntity temp = link.block;
-//                temp.section = section;
-//                section.blocks.add(temp);
-//                temp.order = section.blocks.size();
-//            }
-//            */
-//        }
-//        return true;
-//    }
-
     private List<YiAnEntity> mYiAns = null;// new ArrayList<YiAnEntity>();
 }
