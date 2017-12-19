67f4983f145ce3f4ed2dd239b9ca1c8c6c6ac6a7
Hubert <fuqunxing@gmail.com>
2017-12-19 18:44:37 +1300

    create blocks and blockGroup

--- DataGenerator/src/com/hubert/parser/AST/Evaluation/YiAn/YiAnScope.java
+++ DataGenerator/src/com/hubert/parser/AST/Evaluation/YiAn/YiAnScope.java
@@ -12,32 +12,32 @@ public class YiAnScope {
     public YiAnScope(Storage storage) {
         mStorage = storage;
     }
-    
-    public DataProvider getDataProvider(){
+
+    public DataProvider getDataProvider() {
         return getVariable(YiAnScope.RawContentProvider);
     }
-    
-    public HerbAliasManager getHerbAliasManager(){
+
+    public HerbAliasManager getHerbAliasManager() {
         return getVariable(YiAnScope.HerbAliasManagerKey);
     }
 
-    public SectionEntity getRootSection(){
+    public SectionEntity getRootSection() {
         return getVariable(YiAnScope.RootSectionKey);
     }
 
-    public SectionEntity getActiveSection(){
+    public SectionEntity getActiveSection() {
         SectionEntity section = getVariable(YiAnScope.ActiveSectionEntityKey);
-        if (section != null){
+        if (section != null) {
             return section;
         }
         return getRootSection();
     }
-    
-    public BlockPositionManager getBlockPositionManager(){
+
+    public BlockPositionManager getBlockPositionManager() {
         return getVariable(YiAnScope.BlockPositionManagerKey);
     }
-    
-    public BookEntity getBook(){
+
+    public BookEntity getBook() {
         return getVariable(YiAnScope.BookKey);
     }
 
@@ -48,12 +48,12 @@ public class YiAnScope {
     public void setYiAn(YiAnEntity yiAn) {
         setRawVariable(YiAnKey, yiAn);
     }
-    
-    public BlockLinkCreator<YiAnDetailEntity> getYiAnDetail(){
+
+    public BlockLinkCreator<YiAnDetailEntity> getYiAnDetail() {
         return getVariable(YiAnDetailKey);
     }
-    
-    public void setYiAnDetail(YiAnDetailEntity value){
+
+    public void setYiAnDetail(YiAnDetailEntity value) {
         setVariable(YiAnDetailKey, value, YiAnNodeConstants.Description);
     }
 
@@ -62,57 +62,67 @@ public class YiAnScope {
     }
 
     public BlockLinkCreator<YiAnPrescriptionEntity> setYiAnPrescription(YiAnPrescriptionEntity value) {
+        List<YiAnPrescriptionEntity> prescriptions = getPrescriptions();
+        prescriptions.add(value);
         return setVariable(YiAnPresciption, value, YiAnNodeConstants.RecipeDetail);
     }
-    
-    public YiAnPrescriptionItemEntity getYiAnPrescriptionItem(){
+
+    public YiAnPrescriptionItemEntity getYiAnPrescriptionItem() {
         return getVariable(YiAnPrescriptionItem);
     }
-    
-    public void setYiAnPrescriptionItem(YiAnPrescriptionItemEntity value){
+
+    public void setYiAnPrescriptionItem(YiAnPrescriptionItemEntity value) {
         setRawVariable(YiAnPrescriptionItem, value);
     }
-    
-    public List<SortedMap<Position, String>> getOriginalTokens(){
+
+    public List<SortedMap<Position, String>> getOriginalTokens() {
         return getVariable(YiAnScope.OriginalTokenKey);
     }
-    
-    public void initCurrentTokens(){
+
+    public void initCurrentTokens() {
         SortedMap<Position, String> tokens = new TreeMap<Position, String>();
         setRawVariable(YiAnScope.YiAnTokens, tokens);
-        
+
         List<SortedMap<Position, String>> all = getOriginalTokens();
         all.add(tokens);
     }
-    
-    public SortedMap<Position, String> getCurrentTokens(){
+
+    public SortedMap<Position, String> getCurrentTokens() {
         return getVariable(YiAnScope.YiAnTokens);
     }
-    
-    public BlockGroupCreator getBlockGroupCreator(){
+
+    public BlockGroupCreator getBlockGroupCreator() {
         return getVariable(BlockGroupCreatorKey);
     }
-    
+
     public BlockGroupCreator createBlockGroupCreator(BlockGroupTypeEnum type) {
-        BlockGroupCreator creator = new BlockGroupCreator(getBlockGroupCreator(), type,
-                getBlockPositionManager(), getCurrentTokens());
-        
+        BlockGroupCreator creator = new BlockGroupCreator(getBlockGroupCreator(), type, getBlockPositionManager(),
+                getCurrentTokens());
+
         setRawVariable(BlockGroupCreatorKey, creator);
         return creator;
     }
-    
-    public BlockCreator createBlockCreator(BlockTypeEnum type){
-        BlockCreator creator = new BlockCreator(type, getDataProvider(), getActiveSection());
-        
+
+    public BlockCreator createBlockCreator(BlockTypeEnum type) {
+        BlockCreator creator = new BlockCreator(type, getDataProvider(), getBlockPositionManager(), getActiveSection());
+
         BlockGroupCreator blockGroupCreator = getBlockGroupCreator();
         blockGroupCreator.add(creator);
-        
+
         setRawVariable(BlockCreatorKey, creator);
         return creator;
     }
-    
-    public BlockCreator getBlockCreator(){
+
+    public BlockCreator getBlockCreator() {
         return getVariable(BlockCreatorKey);
+    }
+
+    public List<BlockGroupEntity> getBlockGroups() {
+        return getVariable(BlockGroupKey);
+    }
+
+    protected List<YiAnPrescriptionEntity> getPrescriptions() {
+        return getVariable(PrescriptionKey);
     }
 
     protected <T> T getVariable(String key) {
@@ -122,14 +132,13 @@ public class YiAnScope {
     protected <T> T setRawVariable(String key, T value) {
         return mStorage.setVariable(key, value);
     }
-    
+
     protected <T> BlockLinkCreator<T> setVariable(String key, T value, String blockType) {
         BlockLinkCreator<T> provider = new BlockLinkCreator<>(value, blockType, this);
         mStorage.setVariable(key, provider).getEntity();
         return provider;
     }
 
-    
     public final static String RawContentProvider = "Global.RawContentProvider";
     public final static String HerbAliasManagerKey = "Global.HerbAliasManager";
     public final static String OriginalTokenKey = "Global.OriginalToken";
@@ -137,18 +146,22 @@ public class YiAnScope {
     public final static String BookKey = "Global.Book";
     public final static String ActiveSectionEntityKey = "Global.SectionEntity";
     public final static String BlockPositionManagerKey = "Global.BlockPositionManager";
-    
+
+    public final static String BlockGroupKey = "Global.BlockGroup";
+    public final static String PrescriptionKey = "Global.PrescriptionKey";
+
     private Storage mStorage;
 
     private final static String YiAnKey = "YiAn";
     private final static String YiAnDetailKey = "YiAnDetail"; // TODO: delete
-    private final static String YiAnPresciption = "YiAnPrescription"; 
+    private final static String YiAnPresciption = "YiAnPrescription";
     private final static String YiAnPrescriptionItem = "YiAnPrescriptionItem";
     private final static String YiAnTokens = "YiAnTokens";
-    
-    private final static String YiAnBlockGroupKey = "YiAnBlockGroup";
-    private final static String YiAnDetailBlockGroupKey = "YiAnDetailBlockGroup";
-    
+
+    // private final static String YiAnBlockGroupKey = "YiAnBlockGroup";
+    // private final static String YiAnDetailBlockGroupKey =
+    // "YiAnDetailBlockGroup";
+
     private final static String BlockGroupCreatorKey = "BlockGroupCreator";
     private final static String BlockCreatorKey = "BlockCreatorKey";
 }
