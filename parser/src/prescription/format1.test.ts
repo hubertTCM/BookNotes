f5ae0e073ac54242c7133f27384a732b5fe40173
HubertTCM <fuqunxing@gmail.com>
2019-12-23 11:28:51 +1300

    format

--- parser/src/prescription/format1.test.ts
+++ parser/src/prescription/format1.test.ts
@@ -10,149 +10,63 @@ describe("format1", () => {
     const testDataSet: TestData[] = [
       {
         source: "生姜",
-        expectedTokens: [
-          {
-            type: "herb",
-            value: "生姜"
-          }
-        ]
+        expectedTokens: [{ type: "herb", value: "生姜" }]
       },
       {
         source: "生姜 三七各三两",
         expectedTokens: [
-          {
-            type: "herb",
-            value: "生姜"
-          },
-          {
-            type: "herb",
-            value: "三七"
-          },
-          {
-            type: "applyQuantityToPrevious",
-            value: "各"
-          },
-          {
-            type: "number",
-            value: "三"
-          },
-          {
-            type: "uom",
-            value: "两"
-          }
+          { type: "herb", value: "生姜" },
+          { type: "herb", value: "三七" },
+          { type: "applyQuantityToPrevious", value: "各" },
+          { type: "number", value: "三" },
+          { type: "uom", value: "两" }
         ]
       },
       {
         source: "生姜三两",
         expectedTokens: [
-          {
-            type: "herb",
-            value: "生姜"
-          },
-          {
-            type: "number",
-            value: "三"
-          },
-          {
-            type: "uom",
-            value: "两"
-          }
+          { type: "herb", value: "生姜" },
+          { type: "number", value: "三" },
+          { type: "uom", value: "两" }
         ]
       },
       {
         source: "生姜三两（切）",
         expectedTokens: [
-          {
-            type: "herb",
-            value: "生姜"
-          },
-          {
-            type: "number",
-            value: "三"
-          },
-          {
-            type: "uom",
-            value: "两"
-          },
-          {
-            type: "comment",
-            value: "切"
-          }
+          { type: "herb", value: "生姜" },
+          { type: "number", value: "三" },
+          { type: "uom", value: "两" },
+          { type: "comment", value: "切" }
         ]
       },
       {
         source: "生姜（切）",
         expectedTokens: [
-          {
-            type: "herb",
-            value: "生姜"
-          },
-          {
-            type: "comment",
-            value: "切"
-          }
+          { type: "herb", value: "生姜" },
+          { type: "comment", value: "切" }
         ]
       },
       {
         source: "三七三两五钱",
         expectedTokens: [
-          {
-            type: "herb",
-            value: "三七"
-          },
-          {
-            type: "number",
-            value: "三"
-          },
-          {
-            type: "uom",
-            value: "两"
-          },
-          {
-            type: "number",
-            value: "五"
-          },
-          {
-            type: "uom",
-            value: "钱"
-          }
+          { type: "herb", value: "三七" },
+          { type: "number", value: "三" },
+          { type: "uom", value: "两" },
+          { type: "number", value: "五" },
+          { type: "uom", value: "钱" }
         ]
       },
       {
         source: "三七三两五钱 生姜七两",
         expectedTokens: [
-          {
-            type: "herb",
-            value: "三七"
-          },
-          {
-            type: "number",
-            value: "三"
-          },
-          {
-            type: "uom",
-            value: "两"
-          },
-          {
-            type: "number",
-            value: "五"
-          },
-          {
-            type: "uom",
-            value: "钱"
-          },
-          {
-            type: "herb",
-            value: "生姜"
-          },
-          {
-            type: "number",
-            value: "七"
-          },
-          {
-            type: "uom",
-            value: "两"
-          }
+          { type: "herb", value: "三七" },
+          { type: "number", value: "三" },
+          { type: "uom", value: "两" },
+          { type: "number", value: "五" },
+          { type: "uom", value: "钱" },
+          { type: "herb", value: "生姜" },
+          { type: "number", value: "七" },
+          { type: "uom", value: "两" }
         ]
       }
     ];
