eefcb49244c5bcd6d2a8ce48a8147cdef91f631b
HubertTCM <fuqunxing@gmail.com>
2019-12-24 14:53:02 +1300

    covert token to prescription

--- parser/src/prescription/format1.test.ts
+++ parser/src/prescription/format1.test.ts
@@ -1,8 +1,19 @@
-import { Token, parseTokens } from "./format1";
+import { Token, parseTokens, token2PrescriptionItems, tryParsePrescription } from "./format1";
+import { PrescriptionItem } from "./type";
 
 type TestData = {
   source: string;
   expectedTokens: Token[];
+};
+
+type TestTokens = {
+  source: Token[];
+  expected: PrescriptionItem[];
+};
+
+type TestPrescriptionItems = {
+  source: string;
+  expected: PrescriptionItem[];
 };
 describe("format1", () => {
   it("get tokens", () => {
@@ -73,6 +84,52 @@ describe("format1", () => {
     testDataSet.forEach(({ source, expectedTokens }) => {
       var actualResult = parseTokens(source);
       expect(actualResult).toEqual(expectedTokens);
+    });
+  });
+
+  it("tokens to prescription itmes", () => {
+    const testDataSet: TestTokens[] = [
+      {
+        source: [
+          { type: "herb", value: "三七" },
+          { type: "number", value: "三" },
+          { type: "uom", value: "两" },
+          { type: "number", value: "五" },
+          { type: "uom", value: "钱" },
+          { type: "herb", value: "生姜" },
+          { type: "number", value: "七" },
+          { type: "uom", value: "两" }
+        ],
+        expected: [
+          { herb: "三七", quantity: { uom: "两", value: 8 } },
+          { herb: "生姜", quantity: { uom: "两", value: 7 } }
+        ]
+      }
+    ];
+    testDataSet.forEach(({ source, expected }) => {
+      var actualResult = token2PrescriptionItems(source, quantity => {
+        return { ...quantity, uom: "两" };
+      });
+      expect(actualResult).toEqual(expected);
+    });
+  });
+
+  it("text to prescription itmes", () => {
+    const testDataSet: TestPrescriptionItems[] = [
+      {
+        source: "三七三两 生姜七两",
+        expected: [
+          { herb: "三七", quantity: { uom: "两", value: 3 } },
+          { herb: "生姜", quantity: { uom: "两", value: 7 } }
+        ]
+      }
+    ];
+
+    testDataSet.forEach(({ source, expected }) => {
+      var actualResult = tryParsePrescription(source, quantity => {
+        return { ...quantity, uom: "两" };
+      });
+      expect(actualResult).toEqual(expected);
     });
   });
 });
--- parser/src/prescription/format1.ts
+++ parser/src/prescription/format1.ts
@@ -1,12 +1,7 @@
 // 生姜三两（切）　大枣十二枚（擘）生姜一两六铢（切）杏仁十六个（去皮尖）白术　茯苓各三两
 // 生姜 大枣
-import {
-  numberKeyWords,
-  NumberKeyWordType,
-  uomKeyWords,
-  UOMKeyWordType
-} from "./type";
-import { findHerb } from "./utils";
+import { numberKeyWords, NumberKeyWordType, uomKeyWords, UOMKeyWordType, PrescriptionItem, Quantity } from "./type";
+import { findHerb, toNumber } from "./utils";
 
 export type Token = {
   type: "herb" | "number" | "uom" | "comment" | "applyQuantityToPrevious";
@@ -46,9 +41,7 @@ export const parseTokens = (text: string) => {
     }
     if (char === "）") {
       if (currentTokenType !== "comment") {
-        throw new Error(
-          `unexpected token ${char} currentTokenType:${currentTokenType}`
-        );
+        throw new Error(`unexpected token ${char} currentTokenType:${currentTokenType}`);
       }
       tokens.push({ type: "comment", value: currentTokenValue });
       currentTokenValue = "";
@@ -89,4 +82,98 @@ export const parseTokens = (text: string) => {
     ++i;
   }
   return tokens;
+};
+
+export const token2PrescriptionItems = (
+  tokens: Token[],
+  convertUOM: (quanity: Quantity) => Quantity
+): PrescriptionItem[] | null => {
+  if (!tokens) {
+    return null;
+  }
+  let items: PrescriptionItem[] = [];
+  let currentItem: PrescriptionItem | undefined = undefined;
+  let currentNumber: number | undefined = undefined;
+  let applyQuantityToPrevious: boolean = false;
+  for (let i = 0; i < tokens.length; ++i) {
+    const token = tokens[i];
+    switch (token.type) {
+      case "herb":
+        if (currentItem) {
+          currentNumber = undefined;
+
+          if (applyQuantityToPrevious) {
+            if (!currentItem.quantity) {
+              throw new Error(`not quantity when applyQuantityToPrevious is true. herb:${currentItem.herb}`);
+            }
+            for (let j = items.length - 1; j >= 0; --j) {
+              const temp = items[j];
+              if (temp.quantity !== undefined) {
+                break;
+              }
+              temp.quantity = { ...currentItem.quantity };
+            }
+          }
+
+          items.push(currentItem);
+        }
+        currentItem = { herb: token.value };
+        applyQuantityToPrevious = false;
+        break;
+      case "comment":
+        if (!currentItem) {
+          throw new Error(`unkown prescription. ${token.type} ${token.value}`);
+        }
+        if (currentItem.comment) {
+          throw new Error(`comment already set. prescrition:${JSON.stringify(currentItem)} comment ${token.value}`);
+        }
+        currentItem.comment = token.value;
+        break;
+      case "number":
+        if (!currentItem) {
+          throw new Error(`unkown prescription. ${token.type} ${token.value}`);
+        }
+        currentNumber = toNumber(token.value as NumberKeyWordType);
+        break;
+      case "uom":
+        if (!currentItem) {
+          throw new Error(`unkown prescription. ${token.type} ${token.value}`);
+        }
+        const quanity = convertUOM({
+          uom: token.value as UOMKeyWordType,
+          value: currentNumber !== undefined ? currentNumber : 1
+        });
+        if (!currentItem.quantity) {
+          currentItem.quantity = quanity;
+        } else {
+          if (currentItem.quantity.uom != quanity.uom) {
+            throw new Error(`uom not match: ${quanity.uom} not match ${currentItem.quantity.uom}`);
+          }
+          currentItem.quantity.value += quanity.value;
+        }
+        break;
+      case "applyQuantityToPrevious":
+        applyQuantityToPrevious = true;
+      default:
+        throw new Error(`unkonw token: ${token.type} ${token.value}`);
+    }
+  }
+
+  if (currentItem) {
+    items.push(currentItem);
+  }
+  return items;
+};
+
+export const tryParsePrescription = (
+  text: string,
+  convertUOM: (quanity: Quantity) => Quantity
+): PrescriptionItem[] | null => {
+  try {
+    const tokens = parseTokens(text);
+    return tokens ? token2PrescriptionItems(tokens, convertUOM) : null;
+  } catch (e) {
+    console.log("Error:", e);
+    return null;
+  }
 };
