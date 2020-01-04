cac0d7d904ef1d1ab1d66cb67a63b865065c9c19
Hubert Fu <hubert.fu@pushpay.com>
2020-01-04 16:04:05 +1300

    create 伤寒论.json

--- parser/src/books/shl.ts
+++ parser/src/books/shl.ts
@@ -8,9 +8,22 @@ import { convertUom1 } from "../prescription/convertUom";
 import { toNumber } from "../prescription/utils";
 import { Stack } from "../stack";
 
-export type Prescription = {
+export type Prescription =
+  | {
+      name: string;
+      items: PrescriptionItem[];
+      comment?: string;
+    }
+  | PrescriptionWithoutAccurateItems;
+
+/*
+烧裈散方
+妇人中裈近隐处，取烧作灰。
+上一味，水服方寸匕。日三服。小便即利，阴头微肿，此为愈矣。妇人病，取男子裈烧服。
+*/
+export type PrescriptionWithoutAccurateItems = {
   name: string;
-  items: PrescriptionItem[];
+  description: string;
   comment?: string;
 };
 
@@ -27,7 +40,7 @@ type TiaoWenTextToken = {
   order: number;
 };
 type TextToken = {
-  type: "data" | "presctionName" | "tiaowenComment";
+  type: "data" | "presctionName" | "tiaowenComment" | "prescriptionInaccurateText";
   value: string;
 };
 type PrescriptionItemToken = {
@@ -99,7 +112,6 @@ const createTokens = async (): Promise<Token[]> => {
   const tokens: Token[] = [];
   for await (const rawText of readInterface) {
     const line = rawText.trim();
-    //console.log(rawText);
     if (!line) {
       continue;
     }
@@ -128,13 +140,24 @@ const createTokens = async (): Promise<Token[]> => {
       continue;
     }
 
+    if (line === "妇人中裈近隐处，取烧作灰。") {
+      tokens.push({ type: "prescriptionInaccurateText", value: line });
+      continue;
+    }
+
     tokens.push({ type: "data", value: line });
   }
   return tokens;
 };
 
 const createTiaowen = (astStack: Stack<ASTNode>): TiaoWen | undefined => {
+  if (astStack.count() === 0) {
+    return undefined;
+  }
+
   let node = astStack.pop();
+  let order: number = 0;
+  let tiaowenText: string = "";
   let tiaowenComment: string = "";
   const prescriptions: Prescription[] = [];
   while (node) {
@@ -143,7 +166,9 @@ const createTiaowen = (astStack: Stack<ASTNode>): TiaoWen | undefined => {
         if (astStack.count() > 0) {
           throw new Error("");
         }
-        return { order: node.order, text: node.value, prescriptions, comment: tiaowenComment };
+        order = node.order;
+        tiaowenText = node.value;
+        break;
       case "prescription":
         prescriptions.push(node.value);
         break;
@@ -155,8 +180,7 @@ const createTiaowen = (astStack: Stack<ASTNode>): TiaoWen | undefined => {
     }
     node = astStack.pop();
   }
-
-  return undefined;
+  return { order, text: tiaowenText, prescriptions, comment: tiaowenComment };
 };
 
 const token2Tiaowen = (tokens: Token[]): TiaoWen[] => {
@@ -174,6 +198,24 @@ const token2Tiaowen = (tokens: Token[]): TiaoWen[] => {
         break;
       case "presctionName":
         astStack.push({ type: "prescriptionName", value: token.value });
+        break;
+      case "prescriptionInaccurateText":
+        const temp = astStack.pop();
+        if (temp === undefined) {
+          throw new Error(`no name for prescription: ${JSON.stringify(token)}`);
+        }
+        if (temp.type !== "prescriptionName") {
+          throw new Error(
+            `no name for prescription. prescription "${JSON.stringify(token)}" node:"${JSON.stringify(temp)}"`
+          );
+        }
+        astStack.push({
+          type: "prescription",
+          value: {
+            name: temp.value,
+            description: token.value
+          }
+        });
         break;
       case "prescriptionItems":
         const nameNode = astStack.pop();
@@ -194,7 +236,7 @@ const token2Tiaowen = (tokens: Token[]): TiaoWen[] => {
         });
         break;
       case "data":
-        const prescriptionNode = astStack.pop();
+        const prescriptionNode = astStack.top();
         if (prescriptionNode === undefined) {
           throw new Error(`no prescription for: "${JSON.stringify(token)}"`);
         }
@@ -230,6 +272,6 @@ const token2Tiaowen = (tokens: Token[]): TiaoWen[] => {
 export const exportShl = async (): Promise<TiaoWen[]> => {
   const tokens = await createTokens();
   const tiaowen = token2Tiaowen(tokens);
-  console.log(JSON.stringify(tiaowen));
+  fs.writeFileSync("resource/伤寒论.json", `${JSON.stringify(tiaowen, null, 2)}`);
   return tiaowen;
 };
