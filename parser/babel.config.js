49969908274a6526c0073600ab072ce9a71116be
HubertTCM <fuqunxing@gmail.com>
2020-01-16 15:17:52 +1300

    delete

--- parser/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: [["@babel/env", { modules: "commonjs" }], "@babel/typescript"],
-  plugins: [
-    "@babel/proposal-class-properties",
-    "@babel/proposal-object-rest-spread"
-  ]
-};
